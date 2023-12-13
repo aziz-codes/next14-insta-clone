e77a2d54768c0b2a4bb89bb1e46ff989623ca995
aziz-codes <azizisoffline@gmail.com>
2023-12-13 22:59:16 +0500

    layouts

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div className="text-red-500">dashboard route</div>;
+};
+
+export default page;
--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -1,3 +1,4 @@
+"use client";
 const SideNav = () => {
   return <div>SideNav</div>;
 };
